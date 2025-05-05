0e77871deb8d12ff742b59eaaaa7139063f42253
Alyfer Pedroso <138719366+alyfer-pedroso@users.noreply.github.com>
2025-05-05 14:08:53 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  "https://rfluzljvptiotiiwmzvh.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmbHV6bGp2cHRpb3RpaXdtenZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0MDY1MzgsImV4cCI6MjA2MTk4MjUzOH0.e3aiqQmkBoNWbi1X-DzZCSNL718Urnpvw9ZZrSg-OTE"
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
 );
 
 export default supabase;
