be341e1bfba87482377021d783bfb66acfa20d0c
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2025-05-04 21:55:10 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import { nanoid } from "nanoid";
+
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+
+import pool from "./database/index.js";
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+app.get("/", (_, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const originalUrl = await pool.query("SELECT original_url FROM urls WHERE id = $1", [id]);
+    res.status(200).redirect(originalUrl.rows[0].original_url);
+  } catch (error) {
+    res.status(500).send("<p>This short url does not exist!</p>");
+  }
+});
+
+app.post("/createShorUrl", async (req, res) => {
+  const { original_url } = req.body;
+
+  try {
+    let id;
+    let exists = true;
+
+    while (exists) {
+      id = nanoid(10);
+      const check = await pool.query("SELECT 1 FROM urls WHERE id = $1", [id]);
+      exists = check.rows.length > 0;
+    }
+
+    await pool.query("INSERT INTO urls (id, original_url) VALUES ($1, $2)", [id, original_url]);
+    res.status(200).send({ new_url: `http://localhost:${process.env.PORT}/${id}` });
+  } catch (error) {
+    res.status(500).send({ message: "Something went wrong, please try again!" });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
